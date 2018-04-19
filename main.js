c56ece32c382b319441445ccacb97fa12e219e1a
heso6 <romaniuk99@gmail.com>
2018-04-19 19:24:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
-// Author: Jakob Bähr (bambusource.de)
 
 $(document).ready(function() {
-  // finde alle Parallax Ebenen
+
   var $PxLayers = $(document).find(".parallax");
 
-  // --- PARALLAX VERSCHIEBUNG --- \\
   function Parallax() {
-    // Abstant bis nach oben (scroll)
+  
     var Top = document.documentElement.scrollTop || document.body.scrollTop;
 
-    // für jede Parallax Ebene
     for (i = 0; i < $PxLayers.length; i++) {
-      // definiere Ebene
+ 
       ActiveLayer = $PxLayers[i];
-      // definire Elternelement
+
       ActiveParent = $(ActiveLayer).parent();
-      // Abstand bis nach oben (von Element)
+     
       Distance = ActiveParent.offset().top;
 
-      // Höhe der Parallax Ebene und des Containers
+     
       HeightParent = ActiveParent.height();
       HeightLayer = $(ActiveLayer).height();
       console.log(Top);
 
-      //Scroll = Math.round( (Top/$(ActiveLayer).attr('speed')) + Distance)
+     
       Scroll = Math.round((Top - Distance) / $(ActiveLayer).attr("speed"));
 
       $(ActiveLayer).css({
         transform: "translateY(" + Scroll + "px)",
         "-moz-transform": "translateY(" + Scroll + "px)",
         "-ms-transform": "translateY(" + Scroll + "px)"
-        /*transform: 'translateY('+Scroll+'px)'*/
+  
       });
     }
   }
+
+
 
   $(window).scroll(function() {
     Parallax();
   });
 });
+
+
 
 
